aaf919935aabacb2e56be2f1a69b9b3d8995dd2a
Nabeel Kayed <nabeelkayed25@gmail.com>
2022-03-17 09:21:04 +0200

    Exercise 1

---
+++ Exercise 1.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AllTogether1
+{
+    public class FullExercise1
+    {
+        // The following method should return true if each element in the squares sequence
+        // is equal to the square of the corresponding element in the numbers sequence.
+        // Try to write the entire method using only LINQ method calls, and without writing
+        // any loops.
+        public static bool TestForSquares(IEnumerable<int> numbers, IEnumerable<int> squares)
+        {
+            return numbers.Select(n=> n * n).SequenceEqual(squares);
+        }
+    }
+}
